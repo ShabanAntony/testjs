7a1962c729700afddbd6d6dbc76f0fcaa3ac3e6f
BM <shabanantony@gmail.com>
2020-02-06 01:01:09 +0200

    my first commit

---
+++ Learninig/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+console.log(leftBorderWidth);
+
+let persone = {
+    name: "John",
+    age: 25 
+}
+console.log(persone["name"]);
+
+var massive = ['1', '2', '3', '4'];
+console.log(massive[1]);
+
+let newAlert = alert("Push on 'Ok' button");
+
+let newConfirm = confirm("Are your here?");
+
+let answer = prompt("Do your have 18 years old?", "Yes");
+
+console.log(newAlert);
+console.log(newConfirm);
+
